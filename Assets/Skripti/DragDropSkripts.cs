e5c764b23641d427522fe7707683065be0a4581d
Eduards <eduards.polis2@gmail.com>
2022-03-08 14:18:39 +0200

    Papildinats ar skriptiem un ta talak

---
+++ Assets/Skripti/DragDropSkripts.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Jaimporte, lai lietotu visus I interfeisus
+using UnityEngine.EventSystems;
+
+public class DragDropSkripts : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler {
+	// Velkamajam objektam piestiprinátá komponente
+	private CanvasGroup kanvasGrupa;
+	//Priekś parvietojamá objekta atraśanás vietas uzglabáśanas
+	private RectTransform velkObjRectTransf;
+	//Norade uz objektu skriptu
+	public Objekti objektuSkripts;
+
+	// Use this for initialization
+	void Start () {
+		//Pieklust objekta pastiprinátajai CanvasGroup komponentei
+		kanvasGrupa = GetComponent<CanvasGroup>();
+		//Pieklust objekta recttrasform komponentei
+		velkObjRectTransf = GetComponent<RectTransform> ();
+	}
+
+	public void OnPointerDown(PointerEventData notikums){
+		Debug.Log ("Uzklikśḱináts uz velkama objekta!");
+	}
+
+	public void OnBeginDrag(PointerEventData notikums){
+		Debug.Log ("Uzsakta vilkśana");
+		//Velkot objektu tas paliek caurspidigs
+		kanvasGrupa.alpha = 0.6f;
+		//Lai objektam velkot iet cauri raycast stari
+		kanvasGrupa.blocksRaycasts = false;
+	}
+
+	public void OnDrag(PointerEventData notikums){
+		Debug.Log ("Notiek vilkśana");
+		//Maina objekta x, y koordinates
+		velkObjRectTransf.anchoredPosition += notikums.delta / objektuSkripts.kanva.scaleFactor;
+
+
+	}
+	public void OnEndDrag(PointerEventData notikums){
+		Debug.Log ("Notiek vilkśana");
+	}
+}

990a1b634b22a155e231b742ad6bd39f4170fde0
eduuuards <eduards.polis2@gmail.com>
2022-03-21 15:09:48 +0200

    Pabeigts ar pogam

---
+++ Assets/Skripti/Izvelne.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Izvelne : MonoBehaviour {
+
+	// Use this for initialization
+	public void uzSpeli(){
+		SceneManager.LoadScene ("PilsetasAina", LoadSceneMode.Single);
+		
+	}
+}
